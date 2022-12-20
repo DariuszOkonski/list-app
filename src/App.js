ae896f065fb42e603e9348fb92c7b6ab3e572dea
Dariusz Okoński <darek200180@gmail.com>
2022-12-20 17:50:42 +0100

    initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { people } from "./people";
 
 function App() {
+  console.log(people);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Welcome</header>
     </div>
   );
 }
 
 export default App;
+
+// wyświetlić listę ludzi
+// button -> posortować według nazwy użytkownika
+// wyświetlić awatar
+// https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/ -> reszta adresu w obiekcie
