3d066bf54e8e8d8acfeabad8af3d4fcb07f02106
Dariusz Okoński <darek200180@gmail.com>
2022-12-21 17:23:21 +0100

    add basic styles

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import UsersListOneItem from "./UsersListOneItem";
+import "./UsersList.css";
 
 export default function UsersList(props) {
   return (
-    <ul>
+    <ul className="UsersList">
       {[...props.users]
         .sort((a, b) => a.username.localeCompare(b.username))
         .map((userObj) => (
--- src/components/UsersList/UsersListOneItem.js
+++ src/components/UsersList/UsersListOneItem.js
@@ -1,16 +1,18 @@
 import React from "react";
+import "./UsersListOneItem.css";
 
 export default function UsersListOneItem({ user }) {
   return (
-    <li>
-      <strong>
-        {user.first_name} {user.last_name}
-      </strong>
-      <span>- {user.username}</span>
+    <li key={user.username} className="UsersListOneItem">
       <img
+        className="UsersListOneItem__photo"
         src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${user.picture}`}
-        alt=""
+        alt={`Photo of ${user.first_name}`}
       />
+      <span>
+        {user.first_name} {user.last_name}
+      </span>
+      {/* <span>- {user.username}</span> */}
     </li>
   );
 }
