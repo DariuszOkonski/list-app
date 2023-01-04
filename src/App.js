4f9c243bc767a34a44cabf2a72431a94581ceaad
Dariusz Okoński <darek200180@gmail.com>
2023-01-04 16:56:18 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
+import { useState } from "react";
 import "./App.css";
-import { ColorBox } from "./components/ColorBox/ColorBox";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    const person = {
+      firstName,
+      lastName,
+      age,
+    };
+
+    console.log(person);
+  };
+
   return (
     <div className="App">
-      <ColorBox />
+      <form onSubmit={sendForm}>
+        <p>
+          <label>
+            Imie: <br />
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Nazwisko: <br />
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Wiek: <br />
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(Number(e.target.value))}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Wyslij</button>
+        </p>
+      </form>
     </div>
   );
 }
